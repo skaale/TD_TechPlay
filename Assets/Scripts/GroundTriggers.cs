0f2e6d2483e0c996a77e37d605b4c4bb86e8d6e3
skaale <simulacrumdk@gmail.com>
2017-09-10 22:34:39 +0200

    groundTrigger Cardboard

---
+++ Assets/Scripts/GroundTriggers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class GroundTriggers : MonoBehaviour, IPointerClickHandler 
+{
+	public CreateTowerOnClick player;
+
+	void Start()
+	{
+
+		player = GetComponent<CreateTowerOnClick>();
+	}
+
+	// Will work for clicks and Cardboard trigger presses
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		player.Clicked(eventData.pointerCurrentRaycast.worldPosition);
+	}
+}
